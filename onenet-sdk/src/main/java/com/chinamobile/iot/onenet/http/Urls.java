1cef436e8b3e6aafa921cf7c11dbfcddd806bd70
Shaheers <Sup3rmacyGit>
2019-12-05 10:35:02 +0500

    base

---
+++ onenet-sdk/src/main/java/com/chinamobile/iot/onenet/http/Urls.java
@@ -0,0 +1,11 @@
+package com.chinamobile.iot.onenet.http;
+
+public class Urls {
+
+    public static String sScheme = "https"; //https
+
+    public static String sHost = "209.150.154.128";
+
+    public  static String sPort = "876";
+
+}
